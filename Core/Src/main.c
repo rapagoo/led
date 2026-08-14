d7646c568d027c05b3d7d71045eb8d0b2f7e473c
Sungrok Hong <lilspicypepper@gmail.com>
2026-08-14 15:17:42 +0900

    시리얼로 소통 완료

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,8 +19,8 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "dma.h"
-#include "usart.h"
 #include "gpio.h"
+#include "usart.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -39,8 +39,13 @@ PUTCHAR_PROTOTYPE {
   return ch;
 }
 #define RX_BUF_SIZE 64
+#define LINE_BUF_SIZE 64
 uint8_t usart1_rx_buf[RX_BUF_SIZE];
 uint8_t usart2_rx_buf[RX_BUF_SIZE];
+
+uint8_t line_buf[LINE_BUF_SIZE];
+uint16_t line_len = 0;
+
 uint8_t rx_data;
 uint8_t rx_buf[RX_BUF_SIZE];
 /* USER CODE END Includes */
@@ -101,17 +106,55 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 //   }
 // }
 
+static void ProcessKeyboardInput(uint8_t key) {
+  const uint8_t newline[] = "\r\n";
+  const uint8_t erase[] = "\b \b";
+
+  // Enter: 저장한 한 줄을 상대에게 전송
+  if (key == '\r' || key == '\n') {
+    if (line_len > 0) {
+      HAL_UART_Transmit(&huart1, line_buf, line_len, 100);
+    }
+    HAL_UART_Transmit(&huart1, (uint8_t *)newline, 2, 100);
+    HAL_UART_Transmit(&huart2, (uint8_t *)newline, 2, 100);
+    line_len = 0;
+  }
+  // Backspace: 버퍼와 내 화면에서 마지막 문자 삭제
+  else if ((key == '\b') || (key == 0x7F)) {
+    if (line_len > 0) {
+      line_len--;
+      HAL_UART_Transmit(&huart2, (uint8_t *)erase, 3, 100);
+    }
+  }
+  // 일반 문자: 버퍼에 저장하고 내 화면에만 표시
+  else if ((key >= 0x20) && (key <= 0x7E) && (line_len < LINE_BUF_SIZE)) {
+    line_buf[line_len++] = key;
+    HAL_UART_Transmit(&huart2, &key, 1, 100);
+  }
+}
+
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
-  if (huart->Instance == USART1) {
-    // TX to USART2
-    HAL_UART_Transmit(&huart2, usart1_rx_buf, Size, 100);
-    // SET USART1 back to reception
-    HAL_UARTEx_ReceiveToIdle_DMA(&huart1, usart1_rx_buf, RX_BUF_SIZE);
-  } else if (huart->Instance == USART2) {
-    // TX to USART1
-    HAL_UART_Transmit(&huart1, usart2_rx_buf, Size, 100);
-    // SET USART2 back to reception
+  if (huart->Instance == USART2) {
+    /*
+     * PC에서 받은 키를 먼저 처리
+     * 이 작업이 끝날 때까지 DMA 버퍼는 재사용 X
+     */
+    for (uint16_t i = 0; i < Size; i++) {
+      ProcessKeyboardInput(usart2_rx_buf[i]);
+    }
+
+    /* 처리가 끝난 다음 USART2 수신 재시작 */
     HAL_UARTEx_ReceiveToIdle_DMA(&huart2, usart2_rx_buf, RX_BUF_SIZE);
+
+    __HAL_DMA_DISABLE_IT(huart2.hdmarx, DMA_IT_HT);
+  } else if (huart->Instance == USART1) {
+    /* 상대가 보낸 데이터를 내 PC에 출력 */
+    HAL_UART_Transmit(&huart2, usart1_rx_buf, Size, 100);
+
+    /* 출력이 끝난 다음 USART1 수신 재시작 */
+    HAL_UARTEx_ReceiveToIdle_DMA(&huart1, usart1_rx_buf, RX_BUF_SIZE);
+
+    __HAL_DMA_DISABLE_IT(huart1.hdmarx, DMA_IT_HT);
   }
 }
 
@@ -131,20 +174,21 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
   if (huart->Instance == USART1) {
     HAL_UART_DMAStop(&huart1);
     HAL_UARTEx_ReceiveToIdle_DMA(&huart1, usart1_rx_buf, RX_BUF_SIZE);
+    __HAL_DMA_DISABLE_IT(huart1.hdmarx, DMA_IT_HT);
   } else if (huart->Instance == USART2) {
     HAL_UART_DMAStop(&huart2);
     HAL_UARTEx_ReceiveToIdle_DMA(&huart2, usart2_rx_buf, RX_BUF_SIZE);
+    __HAL_DMA_DISABLE_IT(huart2.hdmarx, DMA_IT_HT);
   }
 }
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
   /* USER CODE BEGIN 1 */
 
@@ -152,7 +196,8 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -174,7 +219,9 @@ int main(void)
   /* USER CODE BEGIN 2 */
   HAL_UARTEx_ReceiveToIdle_DMA(&huart2, usart2_rx_buf, RX_BUF_SIZE);
   HAL_UARTEx_ReceiveToIdle_DMA(&huart1, usart1_rx_buf, RX_BUF_SIZE);
-  //HAL_UART_Receive_DMA(&huart2, rx_buf, RX_BUF_SIZE);
+  __HAL_DMA_DISABLE_IT(huart2.hdmarx, DMA_IT_HT);
+  __HAL_DMA_DISABLE_IT(huart1.hdmarx, DMA_IT_HT);
+  // HAL_UART_Receive_DMA(&huart2, rx_buf, RX_BUF_SIZE);
 
   /* USER CODE END 2 */
 
@@ -189,22 +236,21 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -214,22 +260,20 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
     Error_Handler();
   }
 }
@@ -239,11 +283,10 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
@@ -253,14 +296,13 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line
      number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
