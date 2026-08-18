a5d9501c6a2adbeb69a4ebd0bc81a4851f0a5b8f
Sungrok Hong <lilspicypepper@gmail.com>
2026-08-18 15:30:11 +0900

    Interrupt 방식 비동기 제어 (Interrupt Mode) 실습 완료

--- MyApp/ap/apMain.c
+++ MyApp/ap/apMain.c
@@ -11,23 +11,16 @@
 #include <string.h>
 
 extern ADC_HandleTypeDef hadc1;
-void apInit(void) { uartInit(); }
+void apInit(void) {
+  uartInit();
+  HAL_ADC_Start_IT(&hadc1);
+}
 
 uint32_t adc_multi_values[3] = {0, 0, 0};
 
 void apMain(void) {
   while (1) {
-    HAL_ADC_Start(&hadc1);
-    HAL_ADC_PollForConversion(&hadc1, 100);
-    adc_multi_values[0] = HAL_ADC_GetValue(&hadc1);
 
-    HAL_ADC_PollForConversion(&hadc1, 100);
-    adc_multi_values[1] = HAL_ADC_GetValue(&hadc1);
-
-    HAL_ADC_PollForConversion(&hadc1, 100);
-    adc_multi_values[2] = HAL_ADC_GetValue(&hadc1);
-
-    HAL_ADC_Stop(&hadc1);
-    HAL_Delay(500);
+    HAL_Delay(10);
   }
 }
