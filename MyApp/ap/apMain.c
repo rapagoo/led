9f9f29944bd0838ac0e05cf821e2dede829ae237
Sungrok Hong <lilspicypepper@gmail.com>
2026-08-24 14:49:12 +0900

    hcsr04 apmain에 추가해서 테스트

--- MyApp/ap/apMain.c
+++ MyApp/ap/apMain.c
@@ -3,6 +3,7 @@
 #include "myAdc.h"
 #include "myDht11.h"
 #include "myDs1302.h"
+#include "myHcSr04.h"
 #include "myI2c.h"
 #include "myLcd1602.h"
 #include "myMpu6050.h"
@@ -22,6 +23,7 @@ static mpu6050Data_t mpu_data = {0};
 extern ADC_HandleTypeDef hadc1;
 void apInit(void) {
   uartInit();
+  hcSr04Init();
   adcInit();
   dht11Init();
   lcd1602Init();
@@ -47,7 +49,6 @@ void apMain(void) {
   ssd1306DrawString(8, 3, "STM32 MULTI-SENSOR", SSD1306_COLOR_WHITE);
   ssd1306DrawLine(4, 13, 124, 13, SSD1306_COLOR_WHITE);
   ssd1306Update();
-  
 
   while (1) {
     current_tick = HAL_GetTick();
@@ -56,7 +57,7 @@ void apMain(void) {
       tick_1000 = current_tick;
 
       ds1302GetDateTime(&rtc_time);
-      printf("sec: %02d", rtc_time.sec);
+      printf("sec: %02d\r\n", rtc_time.sec);
     }
 
     if (current_tick - tick_250 >= 250) {
@@ -64,6 +65,14 @@ void apMain(void) {
       adcUpdate();
       dht_status = dht11Read(&dht_data);
       internal_temp = adcGetTemp();
+
+      float distance_cm;
+
+      if (hcSr04Read(&distance_cm)) {
+        printf("Distance: %.2f cm\r\n", distance_cm);
+      } else {
+        printf("HC-SR04 read failed\r\n");
+      }
 
       // lcd1602Clear();
       // lcd1602Cursor(0, 0);
@@ -75,30 +84,29 @@ void apMain(void) {
     if (current_tick - tick_100 >= 100) {
       tick_100 = current_tick;
       if (mpu6050Read(&mpu_data)) {
-        printf(">acc_x:%.3f\r\n>acc_y:%.3f\r\n>acc_z:%.3f\r\n>gyro_x:"
-               "%.3f\r\n>gyro_y: %.3f\r\n>gyro_z: %.3f\r\n",
-               mpu_data.accel_x, mpu_data.accel_y, mpu_data.accel_z,
-               mpu_data.gyro_x, mpu_data.gyro_y, mpu_data.gyro_z);
+        // printf(">acc_x:%.3f\r\n>acc_y:%.3f\r\n>acc_z:%.3f\r\n>gyro_x:"
+        //        "%.3f\r\n>gyro_y: %.3f\r\n>gyro_z: %.3f\r\n",
+        //        mpu_data.accel_x, mpu_data.accel_y, mpu_data.accel_z,
+        //        mpu_data.gyro_x, mpu_data.gyro_y, mpu_data.gyro_z);
 
         char str[32];
         ssd1306FillRect(2, 15, 124, 47, SSD1306_COLOR_BLACK);
-        ssd1306DrawChar(4, 16, '~'+1, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(10, 16, '~'+2, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(16, 16, '~'+3, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(22, 16, '~'+4, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(28, 16, '~'+5, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(34, 16, '~'+6, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(40, 16, '~'+7, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(46, 16, '~'+8, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(52, 16, '~'+9, SSD1306_COLOR_WHITE);
-        ssd1306DrawChar(58, 16, '~'+10, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(4, 16, '~' + 1, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(10, 16, '~' + 2, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(16, 16, '~' + 3, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(22, 16, '~' + 4, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(28, 16, '~' + 5, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(34, 16, '~' + 6, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(40, 16, '~' + 7, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(46, 16, '~' + 8, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(52, 16, '~' + 9, SSD1306_COLOR_WHITE);
+        ssd1306DrawChar(58, 16, '~' + 10, SSD1306_COLOR_WHITE);
         snprintf(str, sizeof(str), "GyroX: %5.2f", mpu_data.gyro_x);
         ssd1306DrawString(5, 25, str, SSD1306_COLOR_WHITE);
         snprintf(str, sizeof(str), "GyroY: %5.2f", mpu_data.gyro_y);
         ssd1306DrawString(5, 35, str, SSD1306_COLOR_WHITE);
         snprintf(str, sizeof(str), "GyroZ: %5.2f", mpu_data.gyro_z);
         ssd1306DrawString(5, 45, str, SSD1306_COLOR_WHITE);
-
 
         ssd1306Update();
       }
